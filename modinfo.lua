17e8091e5d651af953e0639aaedb960e174736e2
nixtux <davehomer@googlemail.com>
2011-09-11 23:41:18 +0100

    update version

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name='Robot Defense (Tech Annihilation)',
   description='The New Generation Of War',
   shortname='TA',
-  version='v2.13-rc9',
+  version='v2.14',
   mutator='',
   game='',
   shortGame='',
