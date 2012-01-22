885e50a9029e3791549cf87f63ec807d489f5601
nixtux <davehomer@googlemail.com>
2012-01-22 12:54:23 +0000

    bump version string

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,14 @@
 return {
-  name='Robot Defense (Tech Annihilation)',
+  name='Robot Defense',
   description='The New Generation Of War',
   shortname='TA',
-  version='V2.16.2 (Tech Annihilation v2.16-r1)',
+  version='v2.16.2 (Tech Annihilation v2.16.2)',
   mutator='',
   game='',
   shortGame='TA',
   modtype=1,
   depend = {
-    'Tech Annihilation v2.16-r1',
+    'Tech Annihilation v2.16.2',
   },
   NTAI = {
     tdfpath='TA';
